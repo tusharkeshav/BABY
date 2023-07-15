3633ab5aa58eaf30516a482962e2f42657df0f4f
TK <43966061+xxxhiddenxx@users.noreply.github.com>
2023-07-15 13:21:17 +0530

    Now logging is based on config

--- logs/Logging.py
+++ logs/Logging.py
@@ -1,10 +1,18 @@
 import logging
 import os
+from config.get_config import get_config
 
 LOG_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Application.log')
+
+log_level_info = {'DEBUG': logging.DEBUG,
+                  'INFO': logging.INFO,
+                  'WARNING': logging.WARNING,
+                  'ERROR': logging.ERROR,
+                  }
 
 FORMAT = "[%(asctime)s %(filename)s->%(funcName)s():%(lineno)s]%(levelname)s: %(message)s"
 logging.basicConfig(filename=LOG_FILE_PATH, format=FORMAT,  encoding='utf-8')
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log_level = log_level_info[get_config('default', 'log_level', fallback='INFO').upper()]
+log.setLevel(log_level)
