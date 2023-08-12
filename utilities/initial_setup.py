0a12e4cfc4d3ab7c5572becc5d281aba60cd04c8
TK <43966061+xxxhiddenxx@users.noreply.github.com>
2023-08-12 22:12:36 +0530

    Added functionality to download voice2json binary

--- utilities/initial_setup.py
+++ utilities/initial_setup.py
@@ -4,14 +4,25 @@ from utilities.train_model import train_model
 from logs.Logging import log
 
 file_path = os.path.abspath(__file__)
+folder_path = os.path.dirname(file_path)
 HOME = os.environ['HOME']
 linux_essential_dependencies = ['python3-tk',
                                 'sox',
-                                'alsa-tools']
+                                'alsa-tools',
+                                'wget',
+                                'espeak',
+                                'jq',
+                                'libstdc++6',
+                                'perl',
+                                'libportaudio2',
+                                'libatlas3-base',
+                                'libffi-dev']
 
 linux_optional_dependencies = ['rfkill',
                                'kdeconnect',
                                'xdotools']
+
+install_broken_dependencies = 'apt install -f'
 
 
 def run(cmd: str, raise_error: bool = True) -> tuple:
@@ -35,24 +46,62 @@ def install_linux_dependencies():
 
 
 def download_profile():
-    cmd = '/usr/bin/voice2json --profile en-us_kaldi-zamia download-profile'
+    log.info('Downloading Voice2json profile')
+    cmd = '/usr/bin/voice2json download-profile'
     run(cmd)
 
     # after download delete sentence file
+    log.info('Creating symlink to correct sentence file')
     os.remove(os.path.join(HOME, '.local/share/voice2json/en-us_kaldi-zamia/sentences.ini'))
-
-    # create symlink to correct sentence file
-    os.symlink(f'{file_path}/sentences.ini', f'{HOME}/.local/share/voice2json/en-us_kaldi-zamia/sentences.ini')
+    #
+    # # create symlink to correct sentence file
+    os.symlink(f'{folder_path}/sentences.ini', f'{HOME}/.local/share/voice2json/en-us_kaldi-zamia/sentences.ini')
+    log.info('Training model')
     train_model()
 
 
 def install_python_requirements():
-    cmd = f'{file_path}/venv/bin/python -m pip install -r {file_path}/requirement.txt'
+    cmd = f'{folder_path}/venv/bin/python -m pip install -r {file_path}/requirement.txt'
     run(cmd)
+
+
+def install_voice2json():
+    log.info('Downloading Voice2json')
+    download_voice2json = f'cd /tmp && wget https://github.com/synesthesiam/voice2json/releases/download/v2.1' \
+                          f'/voice2json_2.1_amd64.deb '
+    run(cmd=download_voice2json)
+
+    log.info('Installing Voice2json')
+    install_voice2json = 'dpkg -i /tmp/voice2json_2.1_amd64.deb'
+    run(cmd=install_voice2json)
+
+    run(cmd=install_broken_dependencies)
+
+    # Libssl and libffi binary hot fix
+    try:
+        log.info('Downloading and installing LibSSL')
+        lib_ssl_download = 'cd /tmp && wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f' \
+                           '-1ubuntu2.19_amd64.deb '
+        run(cmd=lib_ssl_download)
+
+        lib_ssl_binary_install = 'dpkg -i /tmp/libssl1.1_1.1.1f-1ubuntu2.19_amd64.deb'
+        run(cmd=lib_ssl_binary_install)
+        run(cmd=install_broken_dependencies)
+
+        log.info('Downloading and installing Libffi')
+        libffi_binary_download = 'cd /tmp && wget http://archive.ubuntu.com/ubuntu/pool/main/libf/libffi/libffi6_3.2' \
+                                 '.1-8_amd64.deb '
+        libffi_binary_install = 'dpkg -i /tmp/libffi6_3.2.1-8_amd64.deb'
+        run(cmd=libffi_binary_download)
+        run(cmd=libffi_binary_install)
+        run(cmd=install_broken_dependencies)
+    except Exception as e:
+        log.info(f'Error while installing Libssl {e}')
 
 
 def main():
     install_linux_dependencies()
+    install_voice2json()
     download_profile()
     install_python_requirements()
     pass
