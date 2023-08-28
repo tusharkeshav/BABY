8b80c3761d54f0e4798d28f9a8007c2588e6c437
TK <43966061+xxxhiddenxx@users.noreply.github.com>
2023-08-28 12:35:18 +0530

    - Improved mycroft

--- wake_word/mycroft.py
+++ wake_word/mycroft.py
@@ -1,4 +1,5 @@
 from subprocess import getstatusoutput
+from logs.Logging import log
 
 
 class MyCroftWordDetectionExecutionFailed(Exception):
@@ -11,6 +12,8 @@ def _run(cmd):
 
 
 def detect_wake_word():
+    print('*********** Starting Recording and checking for waking word ************')
+    log.debug('*********** Starting Recording and checking for waking word ************')
     cmd = '/usr/bin/voice2json wait-wake --exit-count 1'
     status, output = _run(cmd)
     if status == 0:
